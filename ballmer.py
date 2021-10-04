ccd89d58e50cbc4875e458a8b3fea3cc0ac60f0e
mas <m@stdwtr.io>
2021-10-04 17:47:06 -0400

    Update ballmer to take time into account

--- ballmer.py
+++ ballmer.py
@@ -1,16 +1,32 @@
+from datetime import datetime as dt
 from typing import Tuple, List
 from enum import Enum
 
+DATE = dt.now()
 
 # Modify Me
 data = {
-    "weight": 225,
-    "sex": "m",
-    "time": 0.25,
-    "ingredients": [
-        [1, 0.24],
-        [1, 0.4],
-        [1, 0.16]
+    "weight": 225,      # in pounts
+    "sex": "m",         # m or f
+    "cocktails": [
+        {
+            "name": "Negroni",
+            "time": DATE.replace(hour=17, minute=30),   # 24 hour time
+            "ingredients": [
+                (1, 0.24),  # units (ounces, abv as decimal)
+                (1, 0.4),
+                (1, 0.16)
+            ]
+        },
+        {
+            "name": "Negroni",
+            "time": DATE.replace(hour=17, minute=45),
+            "ingredients": [
+                (1, 0.24),
+                (1, 0.4),
+                (1, 0.16)
+            ]
+        }
     ]
 }
 
@@ -22,8 +38,10 @@ class R(Enum):
 
 
 def calc_bac(alcohol: float, weight: float, t: float, sex: R) -> float:
-    """Takes quantity of alcohol, weight, time since consumed, and enum R."""
-    bac = alcohol / (weight * sex.value)
+    """Takes quantity of alcohol in ounces, weight in pounds, time since
+    consumed in hours, and enum R.
+    """
+    bac = alcohol / (weight * 16 * sex.value)
     return bac - (t * 0.015)
 
 
@@ -36,7 +54,7 @@ def calc_abv(ingredients: List[Tuple[float, float]]) -> float:
     for qty, abv in ingredients:
         total_vol += qty
         numerator += qty * abv
-    return numerator / total_vol
+    return (numerator / total_vol) * 100
 
 
 def calc_alcohol(ingredients: List[Tuple[float, float]]) -> float:
@@ -46,9 +64,14 @@ def calc_alcohol(ingredients: List[Tuple[float, float]]) -> float:
 
 
 if __name__ == "__main__":
-    abv = calc_abv(data["ingredients"])
-    print(f"Your drink had an abv of {abv}")
-    alcohol = calc_alcohol(data["ingredients"])
-    print(f"You consumed {alcohol} alcohol")
-    bac = calc_bac(alcohol, data["weight"], data["time"], getattr(R, data["sex"]))
-    print(f"You have a bac of {bac}")
+    total_bac = 0
+    for drink in data['cocktails']:
+        abv = calc_abv(drink['ingredients'])
+        print(f"Your {drink['name']} had an abv of {abv}")
+        alcohol = calc_alcohol(drink["ingredients"])
+        print(f"That equates to {alcohol} ounces of alcohol.")
+        hours = (DATE-drink["time"]).seconds / 60 / 60
+        bac = calc_bac(alcohol, data["weight"], hours, getattr(R, data["sex"]))
+        print(bac)
+        total_bac += min(bac, 0)
+    print(f"You have a bac of {total_bac}")
